9fcb73643c7e834a50491a3953c1c71286935bb1
mgoldfi1 <mgoldfi1@gmail.com>
2019-05-18 23:52:27 -0700

    more fixes on styling

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import banner from "../assets/images/banner.png";
 import { connect } from "react-redux";
 import { selectTab } from "../actions/index";
