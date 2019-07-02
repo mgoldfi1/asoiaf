9fc22dfa0db5e0a3a4d77f90d9311c787dbab598
mgoldfi1 <mgoldfi1@gmail.com>
2019-07-01 18:44:24 -0700

    added table to books container component

--- src/components/Books.js
+++ src/components/Books.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import "../assets/css/Books.css";
 import { setLoading } from "../actions/index";
 import { sortBooks } from "../actions/index";
+import { Link } from "react-router-dom";
 //LOADING SPINNER
 import { RingLoader } from "react-spinners";
 //Book component
@@ -11,6 +12,9 @@ import Book from "./Book";
 import Button from "@material-ui/core/Button";
 import arrowup from "../assets/images/arrow-up.svg";
 import arrowdown from "../assets/images/arrow-down.svg";
+//table
+import ReactTable from "react-table";
+import "react-table/react-table.css";
 
 const Books = props => {
   const [books, setBooks] = useState(props.books);
@@ -22,20 +26,41 @@ const Books = props => {
   }, []);
 
   useEffect(() => {
+    console.log(props.books);
     setBooks(props.books);
   }, [props.books]);
 
-  const renderBooks = () => {
-    return books.map((book, index) => {
-      return <Book index={index + 1} data={book} />;
-    });
-  };
-
-  const handlePress = (sort, dir) => {
-    props.sortBooks(sort, dir);
-  };
+  // const renderBooks = () => {
+  //   return books.map((book, index) => {
+  //     return <Book index={index + 1} data={book} />;
+  //   });
+  // };
+  const columns = [
+    {
+      Header: "Name",
+      accessor: "name",
+      Cell: ({ row }) => <Link to={"/books/" + row.name}>{row.name}</Link>
+    },
+    {
+      Header: "Published",
+      id: "released",
+      accessor: book => book.released.split("-")[0]
+      // String-based value accessors!
+    },
+    {
+      Header: "Pages",
+      accessor: "numberOfPages" // String-based value accessors!
+    },
+    {
+      Header: "Characters",
+      accessor: "characters.length" // String-based value accessors!
+    }
+  ];
+  // const handlePress = (sort, dir) => {
+  //   props.sortBooks(sort, dir);
+  // };
   return (
-    <div style={{ overflow: "auto" }}>
+    <div style={{ overflow: "auto", width: "100%" }}>
       {props.loading ? (
         <div className="loader">
           <RingLoader />
@@ -52,36 +77,15 @@ const Books = props => {
               fontSize: "24px",
               fontWeight: "700"
             }}
-          >
-            Sort By:{" "}
-            <Button style={{ marginLeft: "50px" }} variant="contained">
-              Release
-            </Button>
-            <img
-              onClick={() => handlePress("release", "up")}
-              src={arrowup}
-              title="uparrow"
-            />
-            <img
-              src={arrowdown}
-              title="downarrow"
-              onClick={() => handlePress("release", "down")}
-            />
-            <Button style={{ marginLeft: "30px" }} variant="contained">
-              Page Count
-            </Button>
-            <img
-              onClick={() => handlePress("pagecount", "up")}
-              src={arrowup}
-              title="uparrow"
-            />
-            <img
-              src={arrowdown}
-              title="downarrow"
-              onClick={() => handlePress("pagecount", "down")}
+          />
+
+          <div className="book-container">
+            <ReactTable
+              defaultPageSize={10}
+              data={props.books}
+              columns={columns}
             />
           </div>
-          <div className="book-container">{renderBooks()}</div>
         </>
       )}
     </div>
